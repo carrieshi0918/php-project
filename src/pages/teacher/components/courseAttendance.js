16ff6730b86ec0ee96ca26b1002df6bc1cc6afa0
carrieshi0918 <syt1124@gmail.com>
2025-03-04 17:52:40 -0500

    Update courseAttendance.js

--- src/pages/teacher/components/courseAttendance.js
+++ src/pages/teacher/components/courseAttendance.js
@@ -1,4 +1,4 @@
-import React from "react";-
+import React from "react";
 import { connect } from "react-redux";
 import { TableContainer, Table, TableHead, TableBody, TableCell, Paper, TableRow } from "@material-ui/core";
 
